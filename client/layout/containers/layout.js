956bf28a9a83101fde3c54c464e1c1ba719ef68e
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2016-07-17 14:15:18 +1200

    styling and package updates

--- client/layout/containers/layout.js
+++ client/layout/containers/layout.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import style from '../styles/layout'
+
 class LayoutContainer extends React.Component {
   render () {
-    return <div>
+    return <div className={style.column}>
       HI
     </div>
   }
 }
 
 export default connect(
-  
+
 )(LayoutContainer)
---
+++ client/layout/styles/layout.js
@@ -0,0 +1,8 @@
+import Stilr from 'stilr'
+
+export default Stilr.create({
+  column: {
+    display: 'flex',
+    flexDirection: 'column'
+  }
+})
