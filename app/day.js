db04353cba20c6345e75293ec28deba6e8e8a3c0
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2016-11-13 23:05:08 +1300

    component for a day, this needs to be renamed

---
+++ app/day.js
@@ -0,0 +1,24 @@
+import React, { PropTypes } from 'react'
+
+import style from './style'
+
+const Day = React.createClass({
+  getInitialState (props) {
+    return {
+      active: false
+    }
+  },
+
+  render () {
+    return (
+      <div
+        onClick={() => { this.setState({ active: !this.state.active })}}
+        className={this.state.active ? style.dayCellActive : style.dayCellInactive}
+      >
+
+      </div>
+    )
+  }
+})
+
+export default Day
