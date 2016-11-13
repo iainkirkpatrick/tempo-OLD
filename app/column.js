836882061064a836e6041c24f1237a934a66c743
Iain Kirkpatrick <kirkpatrick.iain@gmail.com>
2016-11-13 23:04:49 +1300

    column component

---
+++ app/column.js
@@ -0,0 +1,30 @@
+import React, { PropTypes } from 'react'
+import moment from 'moment'
+require('moment-range')
+
+import Day from './day'
+
+import style from './style'
+
+// probably bad manipulation of moment objects
+var now = moment()
+var twoWeeksAhead = moment().add(2, 'weeks')
+var nextTwoWeeks = moment.range(now, twoWeeksAhead).toArray('days')
+console.log(nextTwoWeeks)
+
+const Column = React.createClass({
+  render () {
+    return (
+      <div className={style.gridColumn}>
+        <h4>{this.props.person}</h4>
+        {
+          nextTwoWeeks.map((d) => {
+            return <Day date={d} />
+          })
+        }
+      </div>
+    )
+  }
+})
+
+export default Column
